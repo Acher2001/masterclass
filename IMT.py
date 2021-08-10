3a6cf803cbd66652eeb2fea0f1ea673a91dabc5b
Acher2001 <sasha.festus2@gmail.com>
2021-08-10 12:00:47 +0500

    Первая версия Индеуса Массы Тела

---
+++ IMT.py
@@ -0,0 +1,35 @@
+from tkinter import *
+from tkinter import messagebox
+
+root=Tk()
+
+def clic():
+    
+
+
+root['bg']='#ffffff'  #Вводим фон
+root.title('Индекс Массы Тела')  #Вводим название
+root.wm_attributes('-alpha', 0.85)  #Вводим прозрачность окна
+root.geometry('300x175')  #Вводим размер окна
+
+root.resizable(width=False, height=False)  #Огроничение размера окна
+
+canvas=Canvas(root, height=300, width=175)  #Создоём холст
+canvas.pack()
+
+frame=Frame(root, bg='white')  #Рамка с другими виз. кампонентоми
+frame.place(relx=0.15, rely=0.15, relwidth=0.7, relheight=0.7)  #Обект
+
+title1=Label(frame, text='Введите вес', bg='#990066', font=40)  #Текст
+title1.pack()
+ves=Entry(frame, bg='#fafafa')
+ves.pack()
+title2=Label(frame, text='Введите рост', bg='#990066', font=40)  #Текст
+title2.pack()
+rost=Entry(frame, bg='#fafafa')
+rost.pack()
+btn=Button(frame, text='Расчитать индекс', bg='#fafafa', command=clic)  #Кнопка
+btn.pack()
+
+
+root.mainloop()  #Постояный цикл запуска окна
